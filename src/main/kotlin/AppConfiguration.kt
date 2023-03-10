7eaf23e90fbcebcf62ba9c2cdfcafa0e7fc0d791
Jhonata Da Rocha <jhonata182@gmail.com>
2023-03-10 17:41:20 +0000

    Small refactor to make it more complete

--- src/main/kotlin/AppConfiguration.kt
+++ src/main/kotlin/AppConfiguration.kt
@@ -4,5 +4,5 @@ import io.dropwizard.client.JerseyClientConfiguration
 data class AppConfiguration(
     val appName: String? = "",
     val httpbinBaseUrl: String? = "",
-    val jerseyClient: JerseyClientConfiguration = JerseyClientConfiguration()
+    val jerseyClient: JerseyClientConfiguration? = JerseyClientConfiguration()
 ) : Configuration()
